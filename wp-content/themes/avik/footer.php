ccfb856fc2f96c511c43dde46c6c533c74c7b5cc
Avik <aviksen073@gmail.com>
2025-08-22 20:04:25 +0530

    Test Commit

--- wp-content/themes/avik/footer.php
+++ wp-content/themes/avik/footer.php
@@ -9,6 +9,7 @@
                   <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
                   <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                </ul>
+               hello
             </div>
             <div class="location_text">
                <ul>
